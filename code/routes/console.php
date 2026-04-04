7fe852625d2d07b971aadea20aa8ec8de02db3d5
Utsav Shrestha <gamenic.utsav@gmail.com>
2026-04-04 22:54:32 +0545

    Temporarily schedule full sync at 17:13 UTC

--- code/routes/console.php
+++ code/routes/console.php
@@ -17,3 +17,8 @@ Schedule::command('nepse:floorsheet-sync')
     ->dailyAt((string) config('nepse.sync.floorsheet_schedule_time', '18:45'))
     ->timezone((string) config('app.timezone'))
     ->withoutOverlapping();
+
+Schedule::command('nepse:sync full')
+    ->dailyAt('17:13')
+    ->timezone('UTC')
+    ->withoutOverlapping();
